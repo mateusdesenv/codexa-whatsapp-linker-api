be5f81448c161a41c6c2a6cf5f1ec99450cc59dd
Mateus Camargo <mateus.desenv@gmail.com>
2026-04-29 02:43:03 -0300

    Configurando .env

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 let connectionPromise = null;
 
 async function connectDatabase() {
-  let mongoDbUri = 'mongodb+srv://mateus_db_user:1908@cluster0.ue2kkz4.mongodb.net/codexa-whatsapp-linker?retryWrites=true&w=majority';
   if (mongoose.connection.readyState === 1) {
     return mongoose.connection;
   }
@@ -12,12 +11,12 @@ async function connectDatabase() {
     return connectionPromise;
   }
 
-  if (mongoDbUri) {
+  if (!process.env.MONGODB_URI) {
     throw new Error('A variável MONGODB_URI não foi definida. Configure no .env local ou nas Environment Variables da Vercel.');
   }
 
   connectionPromise = mongoose
-    .connect(mongoDbUri, {
+    .connect(process.env.MONGODB_URI, {
       serverSelectionTimeoutMS: 10000
     })
     .then(() => {
